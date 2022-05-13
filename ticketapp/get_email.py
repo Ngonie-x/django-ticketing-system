937d977839f85e5a933ad4df2c016ccf3e0c8b7d
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2022-05-13 15:45:04 +0200

    Email extraction script

--- ticketapp/get_email.py
+++ ticketapp/get_email.py
@@ -1,77 +1,159 @@
-import imaplib
+from distutils import errors
+import imapclient
+import pyzmail
+import csv
+import json
+import shelve
 import email
-
-# credentials
-username = "ipcconsultants1@gmail.com"
-
-# generated app password
-app_password = "9MG8pxqFZ+?vPnhq"
-
-# https://www.systoolsgroup.com/imap/
-gmail_host = 'imap.gmail.com'
-
-# set connection
-mail = imaplib.IMAP4_SSL(gmail_host)
-
-# login
-mail.login(username, app_password)
-
-# select inbox
-mail.select("INBOX")
-
-# select specific mails
-_, selected_mails = mail.search(None, 'ALL')
-
-for num in selected_mails[0].split()[:10]:
-    _, data = mail.fetch(num, '(RFC822)')
-    _, bytes_data = data[0]
-
-    # convert the byte data to message
-    email_message = email.message_from_bytes(bytes_data)
-    print("\n===========================================")
-
-    # access data
-    print("Subject: ", email_message["subject"])
-    print("To:", email_message["to"])
-    print("From: ", email_message["from"])
-    print("Date: ", email_message["date"])
-    for part in email_message.walk():
-        if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
-            message = part.get_payload(decode=True)
-            print("Message: \n", message)
-            print("==========================================\n")
-            break
+import imaplib
 
 
-class GetEmails:
-    def __init__(self):
-        gmail_host = 'imap.gmail.com'
-        self.mail = imaplib.IMAP4_SSL(gmail_host)
+EMAIL = 'ipcconsultants1@gmail.com'
+PASSWORD = '9MG8pxqFZ+?vPnhq'
 
-    def login(self, username, password):
-        self.mail.login(username, app_password)
 
-    def get_emails(self):
-        self.mail.select("INBOX")
-        # select specific mails
-        _, selected_mails = self.mail.search(None, 'ALL')
+class EmailDownload:
 
-        for num in selected_mails[0].split()[:10]:
-            _, data = mail.fetch(num, '(RFC822)')
-            _, bytes_data = data[0]
+    """This Class Downloads emails in the inbox of a particular gmail account and create a csv file with the information"""
 
-            # convert the byte data to message
-            email_message = email.message_from_bytes(bytes_data)
-            print("\n===========================================")
+    #################################################################################################################################
 
-            # access data
-            print("Subject: ", email_message["subject"])
-            print("To:", email_message["to"])
-            print("From: ", email_message["from"])
-            print("Date: ", email_message["date"])
-            for part in email_message.walk():
-                if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
-                    message = part.get_payload(decode=True)
-                    print("Message: \n", message)
-                    print("==========================================\n")
-                    break
+    def __init__(self, email, password):
+        """Yeah, initializing everything"""
+        self.email = str(email)
+        self.password = str(password)
+
+    #################################################################################################################################
+
+    def login_to_imap_server(self):
+        """Log in to the imap server"""
+
+        # connecting to the server
+        print("Trying to connect to the server")
+
+        try:
+            imapObj = imaplib.IMAP4_SSL('imap.gmail.com')
+            print("Successfully connected to the IMAP server...")
+
+            # Try logging into gmail
+            print("Trying to log in to gmail...")
+
+            try:
+                imapObj.login(self.email, self.password)
+                print("Logged in")
+                self.select_email_uids(imapObj)
+            except Exception as e:
+                print(e)
+                print("Failed to log you in, make sure your password and email are correct \nand that your have enabled non-google apps in the google settings")
+
+        except:
+            print("Failed to connect, probably some network error")
+
+    #################################################################################################################################
+
+    def select_email_uids(self, imap_object):
+        """Select uids for email data to be extracted"""
+        imap_object.select('INBOX')
+        _, self.uids = imap_object.search(None, 'ALL')
+        self.get_email_content_from_uids(imap_object)
+        self.logout_of_imap_server(imap_object)
+
+    #################################################################################################################################
+
+    # TODO: Create a function that calls envelope with args self.uids
+    def get_email_content_from_uids(self, imap_object):
+        """Get email data from the respective email uid"""
+        try:
+            with shelve.open('data') as db:
+                counter = db['counter']
+        except KeyError:
+            counter = 0
+
+        if counter == 0:
+
+            for num in self.uids[0].split()[:20]:
+                _, data = imap_object.fetch(num, '(RFC822)')
+                _, bytes_data = data[0]
+
+                # convert the byte data to message
+                email_message = email.message_from_bytes(bytes_data)
+
+                self.save_data_in_json(email_message)
+        else:
+            for num in self.uids[0].split()[counter:]:
+                _, data = imap_object.fetch(num, '(RFC822)')
+                _, bytes_data = data[0]
+
+                # convert the byte data to message
+                email_message = email.message_from_bytes(bytes_data)
+
+                self.save_data_in_json(email_message)
+
+        print("saving counter")
+        with shelve.open('data') as db:
+            db['counter'] = counter
+
+    #################################################################################################################################
+
+    def save_data_in_csv(self, email_message):
+        """Writing the information to a csv file"""
+
+        subject = email_message["subject"]
+        to = email_message["to"]
+        from_ = email_message["from"]
+        date_ = email_message["date"]
+        for part in email_message.walk():
+            if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
+                message = part.get_payload(decode=True)
+                message = message.decode('utf-8', 'ignore')
+                break
+
+        data_output_file = open('email_data.csv', 'a', newline='')
+        csv_writer = csv.writer(data_output_file)
+
+        csv_writer.writerow([date_, from_, to, subject, message])
+        data_output_file.close()
+
+    #################################################################################################################################
+
+    def save_data_in_json(self, email_message):
+        """Writing the information to a json file"""
+
+        subject = email_message["subject"]
+        to = email_message["to"]
+        from_ = email_message["from"]
+        date_ = email_message["date"]
+        for part in email_message.walk():
+            if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
+                message = part.get_payload(decode=True)
+                message = message.decode('utf-8', 'ignore')
+                break
+
+        # 1. Convert the data into a dictionary
+        email_dict = {
+            "date": date_,
+            "from": from_,
+            "to": to,
+            "subject": subject,
+            "text": message,
+        }
+
+        # 2. Convert the dictionary into json then dump the shit into a json file
+        with open("email_data.json", 'a') as f:
+            f.write(json.dumps(email_dict, sort_keys=True,
+                    indent=4))
+
+    #################################################################################################################################
+
+    def logout_of_imap_server(self, imap_object):
+        """This function logs out of the imap server"""
+
+        print("Logging Out...")
+        imap_object.close()
+        imap_object.logout()
+        print("Logged Out!!")
+
+    #################################################################################################################################
+
+
+EmailDownload(EMAIL, PASSWORD).login_to_imap_server()
